d264566aa6a56aa279168759996f79a36a365bba
adriantanko <adriantanko@adrians-macbook-air.fritz.box>
2022-03-19 21:21:40 +0100

    commit

--- Test/src/ListeL.java
+++ Test/src/ListeL.java
@@ -2,6 +2,7 @@ public class ListeL<T> {
 	
 	static class ElementL<E> {
 		
+		@SuppressWarnings("unused")
 		private E element;
 		private ElementL<E> next;
 		
@@ -28,6 +29,29 @@ public class ListeL<T> {
 			head = newEl;
 		}
 		return newEl;
+		
+	}
+	
+	public ElementL<T> insert(T o, ElementL<T> pred) {
+		
+		ElementL<T> newEl = new ElementL<T>(o);
+		if (pred == null) {
+			newEl.next = head;
+			head = newEl;
+		} else {
+			newEl.next = pred.next;
+			pred.next = newEl;
+		}
+		return newEl;
+	}
+	
+	public void remove(ElementL<T> pred) {
+		
+		if(pred == null && head != null) {
+			head = head.next;
+		} else if(pred.next != null) {
+			pred.next = pred.next.next;
+		}
 		
 	}
 
